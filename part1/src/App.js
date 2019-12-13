2b1137fca09123620a15b8c36635dd1409651580
Carlos Fegurgur <carlos.fegurgur@coyote.com>
2019-12-13 16:13:23 -0600

    updated Part 1 exercises

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
-
+import Header from './Components/Header'
+import Content from './Components/Content'
+import Total from './Components/Total'
 
 const App = () => {
   
-  const course = 'App development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
 
   return(
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
 
   )
