440e4ffffa5e916be31b5307c0475ff04b81f16d
carlosaqf <carlosfegurgur@gmail.com>
2019-12-15 15:51:12 -0600

    added imports to main index file

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import UnicafeApp from './unicafe/UnicafeApp'
 import AnecdotesApp from './anecdotes/AnecdotesApp'
+import AppTwo from './part2/AppTwo'
+import { notes } from './part2/AppTwo'
 
-
-ReactDOM.render(<AnecdotesApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
