9b4255f823bae7a84775c5619331f975f3df789c
carlosaqf <carlosfegurgur@gmail.com>
2019-12-15 15:15:22 -0600

    added Anecdotes App to index file

--- part1/src/index.js
+++ part1/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import UnicafeApp from './unicafe/UnicafeApp'
+import AnecdotesApp from './anecdotes/AnecdotesApp'
 
 
-ReactDOM.render(<UnicafeApp />, document.getElementById('root'));
+ReactDOM.render(<AnecdotesApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
